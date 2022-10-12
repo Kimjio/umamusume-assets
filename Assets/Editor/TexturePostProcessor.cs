47e551ddb3b7d0e67a47734893b52e518e6c74a6
Ji O <kimjioh0927@gmail.com>
2022-10-13 03:39:12 +0900

    Add resources

---
+++ Assets/Editor/TexturePostProcessor.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEditor;
+
+public class TexturePostProcessor : AssetPostprocessor
+{
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        var importer = assetImporter as TextureImporter;
+        importer.wrapMode = TextureWrapMode.Clamp;
+        importer.mipmapEnabled = false;
+    }
+}
