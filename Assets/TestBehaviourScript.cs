8893e8ba13a0dc088ae992100136586389c63e18
Ji O <kimjioh0927@gmail.com>
2023-06-28 00:46:33 +0900

    Update resources

--- Assets/TestBehaviourScript.cs
+++ Assets/TestBehaviourScript.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 
 public class TestBehaviourScript : MonoBehaviour
 {
-	public Action action = delegate() {
-		Console.WriteLine("Hello World!");
-	};
-	
+    public Action action = delegate() {
+        Console.WriteLine("Hello World!");
+    };
+    
     // Start is called before the first frame update
     void Start()
     {
         action.Invoke();
+        StartCoroutine(TestEnumerator());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    IEnumerator TestEnumerator()
+    {
+        Console.WriteLine("1");
+        yield return new WaitForEndOfFrame();
+        Console.WriteLine("2");
+        yield return new WaitForEndOfFrame();
+        Console.WriteLine("3");
+        yield break;
     }
 }
